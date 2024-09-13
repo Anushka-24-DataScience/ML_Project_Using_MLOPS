b580197bfd2e5221c92f4d85dd3076e3f718c9d1
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-09-13 18:11:16 +0530

    logging,exception,utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Prediction",
+    version="0.0.0",
+    author="Anushka Srivastava",
+    author_email="anushka.asthana1983@gmail.com",
+    packages=find_packages()
+)
